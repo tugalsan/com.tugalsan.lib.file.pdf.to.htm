b48160ab92c85ea780fea65fd02a43c2492bfac2
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-07-17 18:59:18 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,2 +1,8 @@
-module com.tugalsan.lib.file.pdf.to.html {
+module com.tugalsan.lib.file.pdf.to.html {requires com.tugalsan.api.os;
+    requires com.tugalsan.api.file;
+    requires com.tugalsan.api.file.properties;
+    requires com.tugalsan.api.unsafe;
+    requires com.tugalsan.api.union;
+    requires com.tugalsan.api.function;
+    requires com.tugalsan.api.log;
 }
